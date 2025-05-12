980c8e361e1bd3ed6df2ec43b86cfae1ea84987d
valentinpersche <valentin.persche@icloud.com>
2025-05-12 22:12:56 +0200

    add sounds and simplified logic in js functions

--- script.js
+++ script.js
@@ -1,68 +1,75 @@
 let currentQuestion = 0;
 let answerGiven = false;
 let correctAnswersCount = 0;
+let AUDIO_RIGHT = new Audio("./assets/audio/right.mp3");
+let AUDIO_WRONG = new Audio("./assets/audio/wrong.mp3");
 
 function init() {
-  document.getElementById("total-questions-number").innerHTML = questions.length;
+  document.getElementById("total-questions-number").innerHTML =questions.length;
   updateProgressBar();
   showQuestion();
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
-  answerGiven = false;
-
-  if (currentQuestion == questions.length - 1) {
-    document.getElementById("next-button").innerHTML = "Quiz beenden";
-  }
-
   if (currentQuestion == questions.length) {
-    finishQuiz();
+    showQuizResults();
   } else {
-    document.getElementById("question-text").innerHTML = question.question;
-    document.getElementById("answer-1").innerHTML = question.answer1;
-    document.getElementById("answer-2").innerHTML = question.answer2;
-    document.getElementById("answer-3").innerHTML = question.answer3;
-    document.getElementById("answer-4").innerHTML = question.answer4;
+    showQuestionAndAnswers();
   }
+}
+
+function showQuestionAndAnswers() {
+  let question = questions[currentQuestion];
+  document.getElementById("question-text").innerHTML = question.question;
+  document.getElementById("answer-1").innerHTML = question.answer1;
+  document.getElementById("answer-2").innerHTML = question.answer2;
+  document.getElementById("answer-3").innerHTML = question.answer3;
+  document.getElementById("answer-4").innerHTML = question.answer4;
 }
 
 function nextQuestion() {
   currentQuestion++;
+  answerGiven = false;
   showQuestion();
-  showCurrentQuestion();
+  questionCounter();
   updateProgressBar();
   resetColors();
-
+  editNextButton();
   document.getElementById("next-button").disabled = true;
 }
 
-function showCurrentQuestion() {
+function questionCounter() {
   document.getElementById("current-question-number").innerHTML =
     currentQuestion + 1;
 }
 
 function checkAnswer(answerIndex) {
   if (answerGiven === true) return;
-
-  let question = questions[currentQuestion];
-  let answer = document.getElementById(`answer-${answerIndex}`);
-  let answerElement = answer.parentElement;
-  let rightAnswer = question.correctAnswer;
-  let rightAnswerElement = document.getElementById(
-    `answer-${rightAnswer}`
-  ).parentElement;
+  
+  let rightAnswer = questions[currentQuestion].correctAnswer;
+  let answerCard = document.getElementById(`answer-${answerIndex}`).parentElement;
+  let rightAnswerCard = document.getElementById(`answer-${rightAnswer}`).parentElement;
 
   if (answerIndex == rightAnswer) {
-    answerElement.style.backgroundColor = "#00ff2d7d";
-    correctAnswersCount++;
+    handleRightAnswer(answerCard, rightAnswerCard);
   } else {
-    answerElement.style.backgroundColor = "#ff00007d";
-    rightAnswerElement.style.backgroundColor = "#00ff2d7d";
+    handleWrongAnswer(answerCard, rightAnswerCard);
   }
 
   document.getElementById("next-button").disabled = false;
   answerGiven = true;
+}
+
+function handleRightAnswer(rightAnswerCard) {
+  rightAnswerCard.style.backgroundColor = "#00ff2d7d";
+  correctAnswersCount++;
+  AUDIO_RIGHT.play();
+}
+
+function handleWrongAnswer(answerCard, rightAnswerCard) {
+  answerCard.style.backgroundColor = "#ff00007d";
+  rightAnswerCard.style.backgroundColor = "#00ff2d7d";
+  AUDIO_WRONG.play();
 }
 
 function resetColors() {
@@ -72,7 +79,13 @@ function resetColors() {
   }
 }
 
-function finishQuiz() {
+function editNextButton() {
+  if (currentQuestion == questions.length - 1) {
+    document.getElementById("next-button").innerHTML = "Quiz beenden";
+  }
+}
+
+function showQuizResults() {
   document.getElementById("quiz-active").style.display = "none";
   document.getElementById("quiz-finished").style.display = "block";
   document.getElementById("quiz-image").src = "./assets/img/well-done.png";
